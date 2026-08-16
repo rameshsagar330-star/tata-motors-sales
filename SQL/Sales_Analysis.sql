d52c73ed82308a6eca6560b60d4bd15cdfe43ad8
T RAMESH <rameshsagar.330@gmail.com>
2026-08-16 12:39:25 +0530

    Refactor SQL queries for better readability

--- SQL/Sales_Analysis.sql
+++ SQL/Sales_Analysis.sql
@@ -31,27 +31,31 @@ Sales Analysis Performed
 */
 
 -- 1. Total revenue tata_motors generated
-  SELECT SUM(sale_price * sale_units) AS total_revenue
+  SELECT 
+	  SUM(sale_price * sale_units) AS total_revenue
   FROM sales_details;
 
 -- 2. Top Selling Models
-  SELECT vehicle_model,
+  SELECT 
+	vehicle_model,
   	SUM(sale_units) AS units_sold
   FROM sales_details
   GROUP BY vehicle_model
   ORDER BY units_sold DESC;
 
 -- 3. Best Selling fuel type (total_units)
-  SELECT v.fuel_type,
+  SELECT 
+	v.fuel_type,
   	SUM(s.sale_units) AS best_selling_fuel_type
   FROM sales_details s
   JOIN vehicle_info v
-  ON s.vin = v.vin
+  	ON s.vin = v.vin
   GROUP BY v.fuel_type			
   ORDER BY best_selling_fuel_type DESC;
 
 -- 4. Revenue by Vehicle model 
-  SELECT vehicle_model,
+  SELECT 
+	vehicle_model,
   	SUM(sale_units * sale_price) AS revenue_by_models
   FROM sales_details
   GROUP BY vehicle_model
@@ -59,55 +63,62 @@ Sales Analysis Performed
 
 -- 5. Monthly sales and trend 
 	-- Monthly sale Trends
-  	SELECT YEAR(sale_date) AS sale_year,
+  	SELECT 
+		YEAR(sale_date) AS sale_year,
   		MONTH(sale_date) AS sale_month,
   		SUM(sale_units * sale_price) AS yearly_revenue
   	FROM sales_details
   	GROUP BY YEAR(sale_date),
-  			    MONTH(sale_date)
+  			MONTH(sale_date)
   	ORDER BY YEAR(sale_date),
-  			    MONTH(sale_date);
+  			MONTH(sale_date);
 	-- yearly sale trend
-  	SELECT YEAR(sale_date) AS sale_year,
+  	SELECT 
+		YEAR(sale_date) AS sale_year,
   		SUM(sale_units * sale_price) AS total_revenue
   	FROM sales_details 
   	GROUP BY YEAR(sale_date)
   	ORDER BY sale_year DESC;
 
 -- 6. Dealer performance
-  SELECT dealer_id,
+  SELECT 
+	dealer_id,
   	SUM(sale_units) AS total_unitssales
   FROM sales_details 
   GROUP BY dealer_id
   ORDER BY total_unitssales DESC;
 
 -- 7. City wise sales
-  SELECT dealer_city,
+  SELECT 
+	dealer_city,
   	SUM(sale_units) AS total_unitssales
   FROM sales_details 
   GROUP BY dealer_city
   ORDER BY total_unitssales DESC;
 
 -- 8. Average selling price
-  SELECT	vehicle_model,
+  SELECT	
+	vehicle_model,
   	AVG(sale_price) AS avg_price
   FROM sales_details
   GROUP BY vehicle_model
   ORDER BY avg_price DESC;
 
 -- 9. Color preferences
-  SELECT vehicle_color,
+  SELECT 
+	vehicle_color,
   	SUM(sale_units) AS total_units
   FROM sales_details
   GROUP BY vehicle_color
   ORDER BY total_units DESC;
 
 --10. Body type preference analysis
-  SELECT v.body_type,
+  SELECT 
+	v.body_type,
   	SUM(s.sale_units) AS total_units
   FROM sales_details s
   JOIN vehicle_info v
-  ON s.vin = v.vin
+  	ON s.vin = v.vin
   GROUP BY v.body_type
   ORDER BY total_units DESC;
 
@@ -124,7 +135,7 @@ WITH cte_group AS(
 		END AS mileage_group
 	FROM sales_details s
 	JOIN vehicle_info v
-	ON s.vin = v.vin
+		ON s.vin = v.vin
 )
 SELECT 
 	mileage_group,
@@ -134,7 +145,8 @@ GROUP BY mileage_group
 ORDER BY mileage_group DESC;
 
 --12. Variant Performance
-  SELECT v.variant,
+  SELECT 
+	v.variant,
   	SUM(s.sale_units) AS total_units
   FROM sales_details s
   JOIN vehicle_info v
