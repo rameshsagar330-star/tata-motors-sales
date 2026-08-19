cd446cbce75f8c8c4bded8651385ac36341dce8e
T RAMESH <rameshsagar.330@gmail.com>
2026-08-19 22:30:09 +0530

    Update Data_Cleaning.sql

--- SQL/Data_Cleaning.sql
+++ SQL/Data_Cleaning.sql
@@ -1,61 +1,107 @@
--- Tata Motors Sales Trend Analysis
 
 		--=================--
 		--  Data Cleaning  --
 		--=================--
-
-	/*
-	-- To change NULL to any values
-	SELECT 
-		sale_id,
-		vin,
-		vehicle_model,
-		vehicle_color,
-		YEAR(manufacture_year) AS manufacture_year,
-		ISNULL(sale_date, '2026-06-01') AS sale_date, 
-		-- replaced the NULL in the date as today date
-		dealer_id,
-		dealer_city,
-		reg_state,
-		ISNULL(sale_units, 0) AS sale_units,
-		ISNULL(sale_price, 250000) AS sale_price
-		-- NULL price is replaced with 2,50,000/- 
-	FROM sales_details;
-
-
-	-- If you find any thing then go with
-	SELECT 
-		sale_id,
-		TRIM(vin) AS vin,
-		TRIM(vehicle_model) AS vehicle_model,
-		TRIM(vehicle_color) AS vehicle_color,
-		YEAR(manufacture_year) AS manufacture_year,
-		ISNULL(sale_date, '2026-06-01') AS sale_date, 
-		-- replaced the NULL in the date as today date
-		dealer_id,
-		TRIM(dealer_city) AS dealer_city,
-		TRIM(reg_state) AS reg_state,
-		ISNULL(sale_units, 0) AS sale_units,
-		ISNULL(sale_price, 250000) AS sale_price
-		-- NULL price is replaced with 2,50,000/- 
-	FROM sales_details;
-	
-	if you find any blank spaces then go with
-	-- For sales_details table
-	SELECT 
-		sale_id,
-		vin,
-		REPLACE(vehicle_model, '', 'N/A') AS vehicle_model,
-		REPLACE(vehicle_color, '', 'N/A') AS vehicle_color,
-		manufacture_year,
-		sale_date,
-		dealer_id,
-		REPLACE(dealer_city, '', 'N/A') AS dealer_city,
-		REPLACE(reg_state, '', 'N/A') AS reg_state,
-		sale_units,
-		sale_price
-	FROM sales_details
-	*/
+INSERT INTO sales_details (
+	sale_id,
+	vin,
+	vehicle_model,
+	vehicle_color,
+	manufacture_year,
+	sale_date,
+	dealer_id,
+	dealer_city,
+	reg_state,
+	sale_units,
+	sale_price
+)
+-- Data cleaning process
+-- Data cleaning for sales_details table
+SELECT 
+	sale_id,
+	vin,
+	CASE WHEN NULLIF(TRIM(vehicle_model), '') IS NULL THEN 'Others'
+ 		 ELSE TRIM(vehicle_model)
+	END AS vehicle_model,
+	CASE WHEN NULLIF(TRIM(vehicle_color), '') IS NULL THEN 'Others'
+ 		 ELSE TRIM(vehicle_color)
+	END AS vehicle_color,
+	YEAR(manufacture_year) AS manufacture_year,
+	CASE WHEN sale_date IS NULL THEN '2026-08-01'
+			WHEN sale_date >= CAST(GETDATE() AS DATE) THEN '2026-08-01'
+			WHEN CAST(sale_date AS DATE) < CAST(manufacture_year AS DATE) THEN '2026-08-01'
+			ELSE sale_date
+	END AS sale_date, -- replaced the NULLS with default date
+	dealer_id,
+	CASE WHEN NULLIF(TRIM(dealer_city), '') IS NULL THEN 'Others'
+		 ELSE TRIM(dealer_city)
+	END AS dealer_city,
+	CASE WHEN NULLIF(TRIM(reg_state), '') IS NULL THEN 'Others'
+		 ELSE TRIM(reg_state)
+	END AS reg_state,
+	CASE WHEN sale_units IS NULL OR sale_units <= 0 THEN 1
+		 ELSE sale_units
+	END AS sale_units, -- if sale_units is 0 then it is replace with minimum value as 1
+	CASE WHEN sale_price IS NULL OR sale_price <= 0 THEN 150000
+		 ELSE sale_price
+	END AS sale_price	-- NULL price is replaced with minimum value as 1,50,000/-
+FROM sales_details
+WHERE sale_id IS NOT NULL;
+--================================
+--================================
+INSERT INTO vehicle_info (
+	record_id,
+	vin,
+	vehicle_model,
+	variant,
+	body_type,
+	vehicle_color,
+	color_code,
+	launch_date,
+	fuel_type,
+	mileage_kmpl,
+	horse_power,
+	safety_rating,
+	seats,
+	air_bags,
+	num_doors
+)
+-- data cleaning for the vehicle_info table
+SELECT 
+	record_id,
+	vin,
+	CASE WHEN NULLIF(TRIM(vehicle_model), '') IS NULL THEN 'Others'
+ 		 ELSE TRIM(vehicle_model)
+	END AS vehicle_model,
+	CASE WHEN NULLIF(TRIM(variant), '') IS NULL THEN 'Others'
+		 ELSE TRIM(variant)
+	END AS variant,
+	CASE WHEN NULLIF(TRIM(body_type), '') IS NULL THEN 'Others'
+		 ELSE TRIM(body_type)
+	END AS body_type,
+	CASE WHEN NULLIF(TRIM(vehicle_color), '') IS NULL THEN 'Others'
+		 ELSE TRIM(vehicle_color)
+	END AS vehicle_color,
+	color_code,
+	COALESCE(launch_date, '2000-01-01') AS launch_date,
+			-- replace null value with 2000-01-01 as default launch date
+	CASE WHEN NULLIF(TRIM(fuel_type), '') IS NULL THEN 'Others'
+		 ELSE TRIM(fuel_type)
+	END AS fuel_type,
+	CASE WHEN UPPER(TRIM(fuel_type)) = 'ELECTRIC' THEN COALESCE(mileage_kmpl, 0)
+		 ELSE COALESCE(mileage_kmpl, 10)
+	END AS mileage_kmpl, -- As per minimum consideration the minimum mileage as 10
+	ISNULL(horse_power, 0) AS horse_power,
+	CASE WHEN safety_rating IS NULL OR safety_rating <= 0 THEN 1
+		 ELSE safety_rating
+	END AS safety_rating, -- replace nulls and 0 with 1 rating values
+	CASE WHEN seats IS NULL OR seats <= 0 THEN 5
+		 ELSE seats
+	END AS seats,	-- replace nulls and 0 with 5 seats 
+	ISNULL(air_bags, 0) AS air_bags, -- replaced nulls with 0 air bags
+	num_doors
+FROM vehicle_info
+WHERE record_id IS NOT NULL;
 
 
 
